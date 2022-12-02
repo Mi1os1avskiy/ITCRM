dace78e85b0027491526cca589322f7180ff7638
@a.miloslavskiy <a.miloslavskiy@brainq.com>
2022-12-02 11:25:17 +0300

    Clients page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class IndexController extends AbstractController
@@ -13,50 +14,17 @@ class IndexController extends AbstractController
             'title' => 'Start page',
         ]);
     }
+
     public function ololo(): Response
     {
         return $this->render('ololo/index.html.twig', []);
     }
-    public function home(): Response
+
+    #[Route('/clients', name: 'clients')]
+    public function clients(): Response
     {
-        return $this->render('home.html.twig', [
-            'title' => 'HOME',
-        ]);
-    }
-    public function dashboard(): Response
-    {
-        return $this->render('dashboard.html.twig', [
-            'title' => 'Dashboard',
-        ]);
-    }
-    public function team(): Response
-    {
-        return $this->render('team.html.twig', [
-            'title' => 'Team',
-        ]);
-    }
-    public function projects(): Response
-    {
-        return $this->render('projects.html.twig', [
-            'title' => 'Projects',
-        ]);
-    }
-    public function calendar(): Response
-    {
-        return $this->render('calendar.html.twig', [
-            'title' => 'Calendar',
-        ]);
-    }
-    public function documents(): Response
-    {
-        return $this->render('documents.html.twig', [
-            'title' => 'Documents',
-        ]);
-    }
-    public function reports(): Response
-    {
-        return $this->render('reports.html.twig', [
-            'title' => 'Reports',
+        return $this->render('clients.html.twig', [
+            'title' => 'Clients',
         ]);
     }
 }
