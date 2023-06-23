71a554c5bd08b97d5379ac7ec8024177ef1f6cf3
@a.miloslavskiy <a.miloslavskiy@brainq.com>
2023-06-23 22:31:30 +0300

    Design updates.

--- src/Controller/Data/UsersController.php
+++ src/Controller/Data/UsersController.php
@@ -57,11 +57,11 @@ class UsersController
                     ['ip' => '2a02:c7f:f64f:5700:b56e:3c00:586', 'date' => '28/01/2023', 'time' => '03:10:13', 'loc' => 'GE']
                 ],
                 'credits' => [
-                    ['date' => '17/02/2023', 'time' => '17:30:02', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'low', 'amount' => 9.99, 'description' => 'Оплата инвойса с входящей транзакции'],
-                    ['date' => '17/02/2023', 'time' => '17:30:02', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'high', 'amount' => 19.99, 'description' => 'Оплата инвойса с баланса клиента'],
-                    ['date' => '17/02/2023', 'time' => '17:30:02', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment' => 'Баланс', 'amount_stat' => 'low', 'amount' => 200, 'description' => 'Credit to the balance from the transaction (balance)'],
-                    ['date' => '17/02/2023', 'time' => '17:30:02', 'country' => 'MSK', 'method' => 'Custom', 'id' => '23202', 'user' => 'Admin', 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'normal', 'amount' => 50, 'description' => 'Оплата инвойса с входящей транзакии'],
-                    ['date' => '17/02/2023', 'time' => '17:30:02', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'low', 'amount' => 999.99, 'description' => 'Оплата инвойса с входящей транзакции'],
+                    ['date' => '17/02/2023', 'time' => '17:30', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment_type' => 1, 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'low', 'amount' => 9.99, 'description' => 'Оплата инвойса с входящей транзакции'],
+                    ['date' => '17/02/2023', 'time' => '17:30', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment_type' => 1, 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'high', 'amount' => 19.99, 'description' => 'Оплата инвойса с баланса клиента'],
+                    ['date' => '17/02/2023', 'time' => '17:30', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment_type' => 0, 'appointment' => 'Баланс', 'amount_stat' => 'low', 'amount' => 200, 'description' => 'Credit to the balance from the transaction (balance)'],
+                    ['date' => '17/02/2023', 'time' => '17:30', 'country' => 'MSK', 'method' => 'Custom', 'id' => '23202', 'user' => 'Admin', 'appointment_type' => 1, 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'normal', 'amount' => 50, 'description' => 'Оплата инвойса с входящей транзакии'],
+                    ['date' => '17/02/2023', 'time' => '17:30', 'country' => 'MSK', 'method' => 'Баланс', 'id' => '', 'user' => '', 'appointment_type' => 1, 'appointment' => 'Инвойс 4833749', 'amount_stat' => 'low', 'amount' => 999.99, 'description' => 'Оплата инвойса с входящей транзакции'],
                 ],
                 'emails' => [
                     ['dept' => 'Служба поддержки', 'title' => 'Tincidunt diam ullamcorper eleifend nullam purus.', 'date' => '17/02/2023', 'time' => '13:17', 'loc' => 'MSK'],
