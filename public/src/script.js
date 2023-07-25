ae6c09f6293dced232659874cd08a7e779837d0e
@a.miloslavskiy <a.miloslavskiy@brainq.com>
2023-07-25 16:36:43 +0300

    ITCRM-17

--- public/src/script.js
+++ public/src/script.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById('2fa').addEventListener('click', () => document.getElementById('2fa_modal').classList.remove('hidden'));
 document.querySelector('[data-pos="2fa_cancel"]').addEventListener("click", () => document.getElementById('2fa_modal').classList.add('hidden'));
 document.querySelector('[data-pos="2fa_disable"]').addEventListener("click", function () {
@@ -16,3 +17,4 @@ document.querySelector('[data-pos="password_change"]').addEventListener("click",
     setTimeout(() => { document.getElementById('password_notification').classList.add('hidden'); }, 3000);
 });
 document.getElementById('password_notification_close').addEventListener("click", () => document.getElementById('password_notification').classList.add('hidden'));
+*/
