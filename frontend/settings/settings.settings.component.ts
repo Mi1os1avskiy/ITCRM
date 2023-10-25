646e10a1ab0c6fe553ffccf4109abe33b19acd8e
@a.miloslavskiy <a.miloslavskiy@brainq.com>
2023-10-25 22:14:06 +0300

    ITCRM-16, 164, 165.

--- frontend/settings/settings.settings.component.ts
+++ frontend/settings/settings.settings.component.ts
@@ -3,6 +3,8 @@ import {PasswordDisplayingMixin, PasswordGenerator} from "is-core-frontend";
 
 const DeleteAdmin = 'delete-admin';
 const AnswerEditor = 'settings-answer-editor';
+const DeleteAnswers = 'settings-delete-answers';
+const DeleteCategory = 'settings-delete-category';
 
 export const SettingsSettingsComponent = {
     data() {
@@ -51,9 +53,62 @@ export const SettingsSettingsComponent = {
             };
             self.$modals(AnswerEditor).show(data)
                 .then((data: any) => {
+                    if ( !!data.id ) {
+                        self.$notifications('global').notify(
+                            new Notification(NotificationLevels.SUCCESS, "Редактирование ответа", "Изменения успешно сохранены")
+                        );
+                    }
+                    else {
+                        self.$notifications('global').notify(
+                            new Notification(NotificationLevels.SUCCESS, "Добавление ответа", "Новый ответ успешно добавлен")
+                        );
+                    }
                 })
                 .catch(() => {
                 });
+        },
+        deleteAnswers() {
+            const self = <any>this;
+            self.$modals(DeleteAnswers).show()
+                .then((data: any) => {
+                    self.$notifications('global').notify(
+                        new Notification(NotificationLevels.SUCCESS, "Удаление ответа", "Ответ успешно удалён")
+                    );
+                })
+                .catch(() => {
+                });
+        },
+        createCategory() {
+            const self = <any>this;
+            self.$notifications('global').notify(
+                new Notification(NotificationLevels.SUCCESS, "Добавление категории", "Новая категория успешно добавлена")
+            );
+        },
+        editCategory() {
+            const self = <any>this;
+            self.$notifications('global').notify(
+                new Notification(NotificationLevels.SUCCESS, "Редактирование категории", "Изменения успешно сохранены")
+            );
+        },
+        deleteCategory() {
+            const self = <any>this;
+            self.$modals(DeleteCategory).show()
+                .then((data: any) => {
+                    self.$notifications('global').notify(
+                        new Notification(NotificationLevels.SUCCESS, "Удаление категории", "Категория успешно удалена")
+                    );
+                })
+                .catch(() => {
+                });
+        },
+        expandAnswer(answerID: string) {
+            const self = <any>this;
+            let id =  answerID;
+            let answer = document.getElementById(id) as HTMLInputElement;
+            answer.classList.toggle('line-clamp-1');
+            let body_id = '.answer_body_' + id;
+            let body = document.querySelector(body_id) as HTMLInputElement;
+            body.classList.toggle('bg-gray-50');
         },
     }
 }
