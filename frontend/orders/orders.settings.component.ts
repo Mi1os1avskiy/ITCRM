4a71a376e59f75e8b3665ec9c0c9468edd006d00
@a.miloslavskiy <a.miloslavskiy@brainq.com>
2023-11-11 14:30:44 +0300

    ITCRM - 21, 33.

--- frontend/orders/orders.settings.component.ts
+++ frontend/orders/orders.settings.component.ts
@@ -1,3 +1,5 @@
+import {Notification, NotificationLevels} from "../misc/notifications/notification";
+
 const OrdersFilters = 'orders-filters';
 const OrdersSpecialSettingsModal = 'orders-special-setting-modal';
 
@@ -35,7 +37,29 @@ export const OrdersSettingsComponent = {
                     txtValue.indexOf(input.value) > -1 ? li[i].style.display = "" : li[i].style.display = "none";
                 }
             });
-        }
+        },
+        orderPromo() {
+            const input = document.getElementById('promo_input') as HTMLInputElement;
+            const btn = document.getElementById('promo_btn') as HTMLButtonElement;
+
+            if (btn.innerText === 'Удалить')
+            {
+               input.value = '';
+               input.removeAttribute('disabled');
+               btn.innerText = 'Применить';
+            }
+            else if (!input.getAttribute('disabled') && input.value !== '')
+            {
+               input.setAttribute('disabled', '');
+               btn.innerText = 'Удалить';
+            }
+        },
+        orderCreating() {
+            const self = <any>this;
+            self.$notifications('global').notify(
+                new Notification(NotificationLevels.SUCCESS, "Создание заказа", "Заказ успешно создан")
+            );
+        },
     }
 }
 
