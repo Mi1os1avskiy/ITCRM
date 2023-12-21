b8a9a670a2c723bb0ecdbc3eccd4de6e9b56cd5f
@a.miloslavskiy <a.miloslavskiy@brainq.com>
2023-12-21 12:55:13 +0300

    ITCRM-134.

--- frontend/billing/billing.settings.component.ts
+++ frontend/billing/billing.settings.component.ts
@@ -35,6 +35,18 @@ export const BillingSettingsComponent = {
                 .catch(() => {
                 });
         },
+        inputSearch() {
+            const input = document.getElementById('clientID') as HTMLInputElement;
+            const ul = document.getElementById('userList') as HTMLElement;
+            const li = ul.getElementsByTagName('li');
+
+            input.addEventListener('keyup', function(event) {
+                for (let i = 0; i < li.length; i++) {
+                    const txtValue = li[i].textContent || li[i].innerText;
+                    txtValue.indexOf(input.value) > -1 ? li[i].style.display = "" : li[i].style.display = "none";
+                }
+            });
+        },
         billingInvoicesFilters() {
             const self = <any>this;
             self.$modals(BillingInvoicesFilters).show()
