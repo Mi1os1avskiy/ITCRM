782ffd444d4b996a9f99848a35ba9392c2c5b255
@a.miloslavskiy <a.miloslavskiy@brainq.com>
2024-01-09 20:14:05 +0300

    ITCRM-34, 164.

--- frontend/settings/settings.settings.component.ts
+++ frontend/settings/settings.settings.component.ts
@@ -67,6 +67,18 @@ export const SettingsSettingsComponent = {
                 .catch(() => {
                 });
         },
+        inputSearch() {
+            const input = document.getElementById('myInput') as HTMLInputElement;
+            const ul = document.getElementById('myUL') as HTMLElement;
+            const li = ul.getElementsByTagName('li');
+
+            input.addEventListener('keyup', function(event) {
+                for (let i = 0; i < li.length; i++) {
+                    const txtValue = li[i].textContent || li[i].innerText;
+                    txtValue.indexOf(input.value) > -1 ? li[i].style.display = "" : li[i].style.display = "none";
+                }
+            });
+        },
         deleteAnswers() {
             const self = <any>this;
             self.$modals(DeleteAnswers).show()
